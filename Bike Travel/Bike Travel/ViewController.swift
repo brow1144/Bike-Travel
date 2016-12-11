0f6897c39b1d237d4fa7aca1ac1afc05adec5843
brow1144 <brow1144@purdue.edu>
2016-12-10 21:25:49 -0500

    Calculate Route Functioning Correctly
    Calculate Route Takes you From Your Current Location to your
    Destination. Todo : Calculate stuff about the travel time / speed.

--- Bike Travel/Bike Travel/ViewController.swift
+++ Bike Travel/Bike Travel/ViewController.swift
@@ -10,12 +10,15 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     var myRoute : MKRoute!
     let manager = CLLocationManager()
-    var pointAnnotation : MKPointAnnotation!
     var pinAnnotationView : MKPinAnnotationView!
     var annotation:MKAnnotation!
     
+    var pointAnnotation : MKPointAnnotation!
+    var myLocation : CLLocationCoordinate2D!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.myMap.delegate = self
         
         //User Location
         manager.delegate = self
@@ -33,67 +36,13 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         
         let location = locations[0]
         let span : MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
-        let myLocation : CLLocationCoordinate2D = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
         let region : MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
         myMap.setRegion(region, animated: true)
         self.myMap.showsUserLocation = true
         
+        
         manager.stopUpdatingLocation()
-    }
-
-    @IBAction func calculateRoute(_ sender: Any) {
-
-        //Set up Route
-        let point1 = MKPointAnnotation()
-        let point2 = MKPointAnnotation()
-        
-        point1.coordinate = CLLocationCoordinate2DMake(40.4267, -86.9267)
-        point1.title = "Hillendbrand Residence Hall"
-        point1.subtitle = "Residence"
-        myMap.addAnnotation(point1)
-        
-        point2.coordinate = CLLocationCoordinate2DMake(40.4277, -86.9170)
-        point2.title = "Lawson Computer Science"
-        point2.subtitle = "Education"
-        myMap.addAnnotation(point2)
-        myMap.centerCoordinate = point2.coordinate
-        myMap.delegate = self
-        
-        //Span of the map
-        myMap.setRegion(MKCoordinateRegionMake(point2.coordinate, MKCoordinateSpanMake(0.03,0.03)), animated: true)
-        
-        let directionsRequest = MKDirectionsRequest()
-        let markTaipei = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point1.coordinate.latitude, point1.coordinate.longitude), addressDictionary: nil)
-        let markChungli = MKPlacemark(coordinate: CLLocationCoordinate2DMake(point2.coordinate.latitude, point2.coordinate.longitude), addressDictionary: nil)
-        
-        directionsRequest.source = MKMapItem(placemark: markChungli)
-        directionsRequest.destination = MKMapItem(placemark: markTaipei)
-        
-        directionsRequest.transportType = MKDirectionsTransportType.walking
-        let directions = MKDirections(request: directionsRequest)
-        
-        directions.calculate(completionHandler: {
-            response, error in
-            
-            if error == nil {
-                self.myRoute = response!.routes[0] as MKRoute
-                self.myMap.add(self.myRoute.polyline)
-            }
-            
-        })
-        
-        //Notifications
-        let content = UNMutableNotificationContent()
-        content.title = "It's been a while!"
-        content.subtitle = "Wana go for a bike ride?"
-        content.body = "Switch over to Bike Travel to get the most accurate bike travel time and current speed while on the bike ride!"
-        content.badge = 1
-        
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 86400, repeats: false)
-        
-        let request = UNNotificationRequest(identifier: "Come Back!", content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
@@ -102,7 +51,9 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
         myLineRenderer.strokeColor = UIColor.green
         myLineRenderer.lineWidth = 3
         return myLineRenderer
+        
     }
+    
     
     @IBAction func textFieldShouldReturn(_ sender: UITextField) {
         if (pointAnnotation != nil) {
@@ -134,6 +85,67 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func calculateRoute(_ sender: Any) {
+        
+        //Set up Route
+        //let point1 = MKPointAnnotation()
+        //let point2 = MKPointAnnotation()
+        
+        /*
+        point1.coordinate = CLLocationCoordinate2DMake(40.4267, -86.9267)
+        point1.title = "Hillendbrand Residence Hall"
+        point1.subtitle = "Residence"
+        myMap.addAnnotation(point1)
+        
+        point2.coordinate = CLLocationCoordinate2DMake(40.4277, -86.9170)
+        point2.title = "Lawson Computer Science"
+        point2.subtitle = "Education"
+        myMap.addAnnotation(point2)
+        myMap.centerCoordinate = point2.coordinate
+        myMap.delegate = self
+ */
+        //myLocation is a CLLocationCoordinate2DMake
+        
+        //Span of the map
+        myMap.setRegion(MKCoordinateRegionMake(myLocation, MKCoordinateSpanMake(0.03,0.03)), animated: true)
+        
+        let directionsRequest = MKDirectionsRequest()
+        let myLocationPlaceMark = MKPlacemark(coordinate: CLLocationCoordinate2DMake(myLocation.latitude, myLocation.longitude), addressDictionary: nil)
+        let destinationPlacemark = MKPlacemark(coordinate: CLLocationCoordinate2DMake(pointAnnotation.coordinate.latitude, pointAnnotation.coordinate.longitude), addressDictionary: nil)
+        
+        directionsRequest.source = MKMapItem(placemark: myLocationPlaceMark)
+        directionsRequest.destination = MKMapItem(placemark: destinationPlacemark)
+        
+        directionsRequest.transportType = MKDirectionsTransportType.walking
+        let directions = MKDirections(request: directionsRequest)
+        
+        directions.calculate(completionHandler: {
+            response, error in
+            
+            if error == nil {
+                self.myRoute = response!.routes[0] as MKRoute
+                self.myMap.add(self.myRoute.polyline)
+            }
+            
+        })
+        
+        
+        
+        
+        //Notifications
+        let content = UNMutableNotificationContent()
+        content.title = "It's been a while!"
+        content.subtitle = "Wana go for a bike ride?"
+        content.body = "Switch over to Bike Travel to get the most accurate bike travel time and current speed while on the bike ride!"
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 86400, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "Come Back!", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
 }
 
